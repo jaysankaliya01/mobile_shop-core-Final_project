562272e869ff85f0578168732da04387790b68ed
Vijay <vijayotaradi11@gmail.com>
2025-01-26 18:47:01 +0530

    Produuct controler

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace mobile_shop_core_.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Shop { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
